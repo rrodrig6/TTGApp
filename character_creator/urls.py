bf85077f1f0fdd2153ad1c721dafa24ef9d37b6b
Rodney Rodriguez <rrodrig6@uccs.edu>
2020-09-16 20:27:05 -0500

    Add views for character pages

--- character_creator/urls.py
+++ character_creator/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('<int:character_id>/', views.sheet, name='sheet'),
+	path('creator/', views.creator, name='creator'),
 ]
 	
--- character_creator/views.py
+++ character_creator/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Character
 
 def index(request):
-	return HttpResponse("Character Creator Index")
+	character_list = Character.objects.all()
+	context = {'character_list': character_list}
+	return render(request, 'character_creator/index.html', context)
+	
+def sheet(request, character_id):
+	context = {'character' : Character.objects.get(id=character_id) }
+	return render(request, 'character_creator/sheet.html', context)
+	
+def creator(request):
+	context = {}
+	return render(request, 'character_creator/creator.html', context)
+	
